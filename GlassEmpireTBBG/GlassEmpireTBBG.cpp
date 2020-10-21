844006c160f258f2d19059c724807c312bc3ae2d
Haider Ali <Haiderali360@gmail.com>
2020-10-21 03:24:10 -0400

    main Improved
    Set up some ideas for how the game could be formatted

--- GlassEmpireTBBG/GlassEmpireTBBG.cpp
+++ GlassEmpireTBBG/GlassEmpireTBBG.cpp
@@ -5,13 +5,31 @@
 
 int main()
 {
-    std::string input;
-    while (true) {
-        std::cin >> input;
-        game(input);
-    }
-}
+	//Sets the initial scene - Menu
+	int scene = 0;
+	std::string input;
 
-void game(std::string input) {
-    
+	//Initialize 4 Players of the Player Class. These will setup during the game set up.
+	Player P1;
+	Player P2;
+	Player P3;
+	Player P4;
+	
+	//Create a pointer array to easily call on the players
+	Player* players[4];
+
+
+	//Load the splash screen from XML and print it
+	gamePrint(); //<--- To be implemented!!!
+
+	//Display the options available to the player from the current scene
+	displayOptions(scene); //<--- To be implemented!!!
+	//Takes player input
+	std::cin >> input;
+	//Performs the action based on the input
+	//Include a pointer to any game elements the actions could change
+	action(input, &scene);
+
+
+
 }
