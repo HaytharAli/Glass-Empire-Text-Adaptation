a0ccad4520ebd1fc43993b40d1a4456107a532e0
Haider Ali <Haiderali360@gmail.com>
2020-10-25 04:35:59 -0400

    Improved Player Class and Added Resource Class

--- GlassEmpireTBBG/Player.h
+++ GlassEmpireTBBG/Player.h
@@ -1,28 +1,38 @@
 #pragma once
 #include <iostream>
-#include <algorithm>
+#include "Resource.h"
 
 class Player
 {
 //private:
-	std::string name;
+	std::string name; // The player name
 	bool isEnforcer; //if true, Enforcer, else a Mover
-	int money;
-	int resources[10]; //When collected, a slot in the array is filled with the correct resource identifier
+	int money; // How much money the player has
+	Resource resources[10];
 	int street;
 	int locOnStreet;
 
+	int getResource(std::string type);
+
 public:
+	std::string getName();
+
 	bool getIsEnforcer();
-	void addResource(int type);
-	void removeResource(int type);
-	bool readyToWin();
+
 	void addMoney(int profit); //Adds cash to player's wallet. Profit can be negative to reduce total, but the balance can't go below zero.
 	int getBalance();
-	std::string getName();
+
+	void addResource(std::string type);
+	int getResourceCost(std::string type); // Returns the value of the resource in the player's inventory
+	void removeResource(std::string type);
+
 	void setStreet(int newStreet);
 	void setLocOnStreet(int newLoc);
 	int getStreet();
 	int getLocOnStreet();
+	
+	bool readyToWin(); // Checks Win Condition, can also be used by player to see what they still need!
+
+	Player(std::string name, bool isEnforcer);
 
 };
