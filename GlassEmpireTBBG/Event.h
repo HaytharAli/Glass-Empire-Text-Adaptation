2021e5026b46a51692b993d1d3fe3ac8bf5c2bd4
Haider Ali <Haiderali360@gmail.com>
2020-10-25 20:24:05 -0400

    Added Event Class
    INCOMPLETE

---
+++ GlassEmpireTBBG/Event.h
@@ -0,0 +1,15 @@
+#pragma once
+
+class Event {
+//Private
+	void gainCash();
+	void moverCash();
+	void enforcerCash();
+	void payDay();
+	void sting();
+	void loseCash();
+	void HandoutCash();
+
+public:
+	void getEvent();
+};
